baf9c114bbe836ee569434bca5657b8017c908e3
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-03-03 15:50:16 +0200

    Defended lab1

--- Source/ProjectGQ/Pickup.cpp
+++ Source/ProjectGQ/Pickup.cpp
@@ -13,9 +13,9 @@ APickup::APickup()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	CustomRoot = CreateDefaultSubobject<USceneComponent>("CustomRoot");
-	CustomMesh = CreateDefaultSubobject<UStaticMeshComponent>("PickupMesh");
-	CollisionBox = CreateDefaultSubobject<UBoxComponent>("CollisionZone");
+	CustomRoot = CreateDefaultSubobject<USceneComponent>(TEXT("CustomRoot"));
+	CustomMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PickupMesh"));
+	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionZone"));
 
 	SetRootComponent(CustomRoot);
 	CustomMesh->SetupAttachment(GetRootComponent());
